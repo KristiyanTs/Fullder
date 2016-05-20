cb575a03238cc1c25734735e822b9be847601e66
Nikola Jichev <njichev@gmail.com>
2016-05-20 20:23:15 +0300

    Added restaurant model.

---
+++ app/models/working_hour.rb
@@ -0,0 +1,2 @@
+class WorkingHour < ActiveRecord::Base
+end
---
+++ db/migrate/20160520171431_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+      t.string :phone_number
+
+      t.timestamps null: false
+    end
+  end
+end
