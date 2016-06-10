02b9d707088285d2e7cc902bb8330ef81417c1f7
kristiyan.c <kristiyan.c@gmail.com>
2016-06-10 19:52:59 +0300

    Cart changes.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,18 +1,15 @@
 class CartsController < ApplicationController
+  def show
+  end
 
-	def show
+  def create
+    @user = current_user
+    @user.create_cart(params[:cart])
+  end
 
-	end
+  private
 
-	def create
-		@user = current_user
-		@user.create_cart(params[:cart])
-	end
-
-	private
-
-	def cart_params
+  def cart_params
     params.require(:cart).permit(:restaurant_id, :table_number, :user_id)
-	end
-
+  end
 end
--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe MealsController, type: :controller do
-
 end
