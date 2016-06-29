ee84b0882a387785a5bbb259f56f074258b625e4
kristiyan.c <kristiyan.c@gmail.com>
2016-06-29 22:31:06 +0300

    .....

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,16 +2,16 @@
 #
 # Table name: orders
 #
-#  id                :integer          not null, primary key
-#  cart_id           :integer
-#  meal_id           :integer
-#  quantity          :integer
-#  specifications    :string
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  meal_size_id      :integer
-#  supplementals_ids :integer
-#  restaurant_id     :integer
+#  id              :integer          not null, primary key
+#  cart_id         :integer
+#  meal_id         :integer
+#  quantity        :integer
+#  specifications  :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  meal_size_id    :integer
+#  supplemental_id :integer
+#  restaurant_id   :integer
 #
 
 FactoryGirl.define do
