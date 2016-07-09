62621621c2078f4f89e5bda35c6b56207b32d2b5
Nikola Jichev <njichev@gmail.com>
2016-07-09 13:16:11 +0300

    Removed restaurant_id from permission roles.

--- app/models/permission_role.rb
+++ app/models/permission_role.rb
@@ -13,5 +13,4 @@
 class PermissionRole < ActiveRecord::Base
   belongs_to :permission
   belongs_to :role
-  belongs_to :restaurant
 end
--- db/migrate/20160708102830_add_column_to_permission_role.rb
+++ db/migrate/20160708102830_add_column_to_permission_role.rb
@@ -2,6 +2,5 @@ class AddColumnToPermissionRole < ActiveRecord::Migration
   def change
     add_column :permission_roles, :permission_id, :integer
     add_column :permission_roles, :role_id, :integer
-    add_column :permission_roles, :restaurant_id, :integer
   end
 end
