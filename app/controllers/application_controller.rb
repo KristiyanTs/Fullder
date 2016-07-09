4941bb2a8cb784ef85bfbf05c8fa49742dc926c8
kristiyan.c <kristiyan.c@gmail.com>
2016-07-09 13:09:30 +0300

    Uncommented application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,19 +11,19 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # protected
+  protected
  
-  # #derive the model name from the controller. egs UsersController will return User
-  # def self.permission
-  #   return name = self.name.gsub('Controller','').singularize.split('::').last.constantize.name rescue nil
-  # end
+  #derive the model name from the controller. egs UsersController will return User
+  def self.permission
+    return name = self.name.gsub('Controller','').singularize.split('::').last.constantize.name rescue nil
+  end
  
-  # def current_ability
-  #   @current_ability ||= Ability.new(current_user)
-  # end
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
  
-  # #load the permissions for the current user so that UI can be manipulated
-  # def load_permissions
-  #   @current_permissions = current_user.role.permissions.collect{|i| [i.subject_class, i.action]}
-  # end
+  #load the permissions for the current user so that UI can be manipulated
+  def load_permissions
+    @current_permissions = current_user.role.permissions.collect{|i| [i.subject_class, i.action]}
+  end
 end
