2f4fd46ec905232f13e6c9c8f086ec743772ed7c
Nikola Jichev <njichev@gmail.com>
2016-07-25 11:35:55 +0300

    Added default values.

--- db/migrate/20160725080611_add_ready_attributes_to_products_and_order_items.rb
+++ db/migrate/20160725080611_add_ready_attributes_to_products_and_order_items.rb
@@ -1,7 +1,7 @@
 class AddReadyAttributesToProductsAndOrderItems < ActiveRecord::Migration
   def change
-    add_column :products, :ready, :boolean
-    add_column :products, :average_time, :datetime
-    add_column :order_items, :ready, :boolean
+    add_column :products, :ready, :boolean, default: false
+    add_column :products, :average_time, :datetime, default: DateTime.current
+    add_column :order_items, :ready, :boolean, default: false
   end
 end
