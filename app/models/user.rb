e8298f046cd4f3f465798e9dca46ffc846a2837a
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-08-02 12:37:52 +0300

    Added allergen tags to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,4 +41,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false }
+
+  acts_as_taggable
+  acts_as_taggable_on :allergens
 end
