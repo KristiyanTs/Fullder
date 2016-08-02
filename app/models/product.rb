1228ac15d45fa047d3e2c3151421d38548d63347
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-08-02 12:33:54 +0300

    Added allergens tags to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -45,4 +45,7 @@ class Product < ActiveRecord::Base
   has_attached_file :avatar, styles: { small: '80x80#', large: '250x250#' }, default_url: '/images/:style/missing.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
+  acts_as_taggable
+  acts_as_taggable_on :allergens
+
 end
