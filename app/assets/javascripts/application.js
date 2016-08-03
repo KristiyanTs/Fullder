cea4b011c5c67313f98acdac364a4893a55293a0
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-08-03 12:05:04 +0300

    Cooler restaurant design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,8 @@ $(document).ready(function() {
   $(".alert").fadeTo(2000, 500).slideUp(500, function(){
       $(".alert").alert('close');
   });
+  $('.restaurant-search-input').tooltip({
+    placement: "bottom",
+    trigger: "focus"
+  });
 });
