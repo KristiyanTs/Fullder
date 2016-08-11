55b224992a16a9744031e464f9e35b72e111b02a
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-08-11 09:44:26 +0300

    Design fixes

--- db/migrate/20160802073015_add_locale_to_users.rb
+++ db/migrate/20160802073015_add_locale_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AddLocaleToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :locale, :string
+    add_column :users, :locale, :string, default: "en"
   end
 end
