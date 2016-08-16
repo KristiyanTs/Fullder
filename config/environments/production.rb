6bba896d0023d6c8692ffa595102cf0e36eddf94
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-08-16 13:34:13 +0300

    Setting up mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fast_order_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
