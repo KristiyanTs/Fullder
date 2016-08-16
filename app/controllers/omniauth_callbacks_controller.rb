3be60b98a20ab7ea4604943206af933efd99880c
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-08-16 14:43:47 +0300

    Added twitter method to sessions_controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -24,6 +24,18 @@ class OmniauthCallbacksController < ApplicationController
     end
   end
 
+  def twitter
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    puts request.env['omniauth.auth']
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Twitter'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session['devise.google_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+
   def failure
     redirect_to root_path
   end
