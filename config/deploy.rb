4273f747ca27733455a134e5c557c8c9f9a8ea9f
Nikola Jichev <njichev@gmail.com>
2016-09-11 15:47:09 +0300

    Change port to ssh port.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '139.59.135.85', port: 80, roles: [:web, :app, :db], primary: true
+server '139.59.135.85', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:Kristiyan96/Fullder.git'
 set :application,     'Fullder'
@@ -18,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/digitalocean_fullder) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
