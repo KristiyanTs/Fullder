85a9171fb9f8e4235c38f42b48019a03d142c0ee
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-09-21 15:49:19 +0100

    Setting that aim to display assets properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+  # Generate digests for assets URLs
+  config.assets.digest = true
 end
