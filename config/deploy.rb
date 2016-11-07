d1ea21761ddd6f5e06887dd7ae5c9270b03390ae
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-11-07 13:41:54 +0000

    Label for restaurant favorites

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,7 +87,6 @@ namespace :deploy do
   end
   
   before :starting,     :check_revision
-  after  :restart { invoke 'sidekiq:restart' }
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
