532b512a48170e5f714598fd71dc2e1b45dc0263
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2016-12-22 12:48:35 +0200

    Fixed displaying issues because of too long strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 module ApplicationHelper
+
+  def shorthand(text, max_symbols)
+    text ||= ""
+    text.length > max_symbols ? text[0,max_symbols-3] + '...' : text
+  end
+
 end
