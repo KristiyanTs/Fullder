718537bd55a87251d9de1e0ca1fa3d272c8e64d0
Kristiyan Tsvetanov <kristiyan.c@gmail.com>
2017-02-08 17:26:18 +0000

    Fixed order of showing items

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,7 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @order_items = current_order.order_items.order('created_at DESC')
+    @order_items = current_order.order_items.order('created_at')
 
     @restaurant = current_order.restaurant
     add_breadcrumb @restaurant.name, restaurant_path(@restaurant), title: "Back to the restaurant"
